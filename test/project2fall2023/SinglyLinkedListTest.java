d6f1ab538b0e10ea3197da318f61fb2741dd4e91
Aron Kabai Tokes <aronkt3458@hotmail.com>
2023-10-25 09:36:55 -0400

    added nested classes for clarity

--- test/project2fall2023/SinglyLinkedListTest.java
+++ test/project2fall2023/SinglyLinkedListTest.java
@@ -1,131 +1,139 @@
 package project2fall2023;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 
 /** tests the SinglyLinkedList class
  *
  * @author Aron Kabai-Tokes
  */
+@RunWith(Enclosed.class)
 public class SinglyLinkedListTest {
 
-    /**
-     * Test of getSize method, of class SinglyLinkedList.
-     */
-    @Test
-    public void testGetSize() {
-        SinglyLinkedList instance = new SinglyLinkedList();
-        int expResult = 0;
-        int result = instance.getSize();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of add method, of class SinglyLinkedList.
-     */
-    @Test
-    public void testAdd() {
-        Container newElement = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(newElement);
-        assertTrue(instance.getSize() == 1);
-        // TODO review the generated test code and remove the default call to fail.
-//        fail("element not added.");
-    }
     
-    @Ignore
-    @Test
-    public void testAddKeepOrder() {
-        Container newElement = new Container("zzzz", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(newElement);
-        instance.add(new Container("aaaa", 10));
-        instance.add(new Container("aab", 10));
-//        assertTrue(instance);
-    }
+    public static class normalClasses {
+        /**
+         * Test of getSize method, of class SinglyLinkedList.
+         */
+        @Test
+        public void testGetSize() {
+            SinglyLinkedList instance = new SinglyLinkedList();
+            int expResult = 0;
+            int result = instance.getSize();
+            assertEquals(expResult, result);
+        }
 
-    /**
-     * Test of exists method, of class SinglyLinkedList.
-     */
-    @Test
-    public void testExists() {
-        Container target = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(target);
-        boolean expResult = true;
-        boolean result = instance.exists(target);
-        assertTrue(expResult == result);
-        
-//        fail("The test case is a prototype.");
-    }
+        /**
+         * Test of add method, of class SinglyLinkedList.
+         */
+        @Test
+        public void testAdd() {
+            Container newElement = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(newElement);
+            assertTrue(instance.getSize() == 1);
+            // TODO review the generated test code and remove the default call to fail.
+    //        fail("element not added.");
+        }
 
-    /**
-     * Test of countOccurences method, of class SinglyLinkedList.
-     */
-    @Test
-    public void testCountOccurences() {
-        SinglyLinkedList<Container> instance = new SinglyLinkedList<>();
-        Container target = new Container("name", 10);
-        instance.add(new Container("name", 10));
-        instance.add(new Container("name", 10));
-        instance.add(new Container("name2", 11));
-        int expResult = 2;   
-        int result = instance.countOccurences(target);
-        assertEquals(expResult, result);
+        @Ignore
+        @Test
+        public void testAddKeepOrder() {
+            Container newElement = new Container("zzzz", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(newElement);
+            instance.add(new Container("aaaa", 10));
+            instance.add(new Container("aab", 10));
+    //        assertTrue(instance);
+        }
+
+        /**
+         * Test of exists method, of class SinglyLinkedList.
+         */
+        @Test
+        public void testExists() {
+            Container target = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(target);
+            boolean expResult = true;
+            boolean result = instance.exists(target);
+            assertTrue(expResult == result);
+        }
+
+        /**
+         * Test of countOccurences method, of class SinglyLinkedList.
+         */
+        @Test
+        public void testCountOccurences() {
+            SinglyLinkedList<Container> instance = new SinglyLinkedList<>();
+            Container target = new Container("name", 10);
+            instance.add(new Container("name", 10));
+            instance.add(new Container("name", 10));
+            instance.add(new Container("name2", 11));
+            int expResult = 2;   
+            int result = instance.countOccurences(target);
+            assertEquals(expResult, result);
+        }
     }
 
     /**
      * Test of remove method, of class SinglyLinkedList.
      */
-    @Test
-    @DisplayName("removing item from head when it is the only item")
-    public void removingItemWhenOnlyOne() {
-        Container target = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(target);
-        boolean expResult = true;
-        boolean result = instance.remove(target);
-        assertTrue(expResult == result);
-    }
-
-    @Test
-    @DisplayName("removing item from head with more than one item")
-    public void removingItemFromHeadWhenMoreThanOne() {
-        Container target = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(target);
-        instance.add(new Container("name2", 11));
-        instance.add(new Container("name2", 12));
-        boolean expResult = true;
-        boolean result = instance.remove(target);
-        assertTrue(expResult == result);
-    }
-
-    @Test
-    @DisplayName("removing item from middle")
-    public void removingItemFromMiddle() {
-        Container target = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(new Container("name", 9));
-        instance.add(target);
-        instance.add(new Container("name2", 11));
-        boolean expResult = true;
-        boolean result = instance.remove(target);
-        assertTrue(expResult == result);
-    }
-
-    @Test
-    @DisplayName("removing item from tail")
-    public void removingItemFromTail() {
-        Container target = new Container("name", 10);
-        SinglyLinkedList<Container> instance = new SinglyLinkedList();
-        instance.add(new Container("name", 8));
-        instance.add(new Container("name2", 9));
-        instance.add(target);
-        boolean expResult = true;
-        boolean result = instance.remove(target);
-        assertTrue(expResult == result);
+    public static class removeItems {
+    
+        @Test
+        @DisplayName("removing item from head when it is the only item")
+        public void removingItemWhenOnlyOne() {
+            Container target = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(target);
+            boolean expResult = true;
+            boolean result = instance.remove(target);
+            assertTrue(expResult == result);
+        }
+        
+        @Test
+        @DisplayName("removing item from head with more than one item")
+        public void removingItemFromHeadWhenMoreThanOne() {
+            Container target = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(target);
+            instance.add(new Container("name2", 11));
+            instance.add(new Container("name2", 12));
+            boolean expResult = true;
+            boolean result = instance.remove(target);
+            assertTrue(expResult == result);
+        }
+        
+        @Test
+        @DisplayName("removing item from middle")
+        public void removingItemFromMiddle() {
+            Container target = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(new Container("name", 9));
+            instance.add(target);
+            instance.add(new Container("name2", 11));
+            boolean expResult = true;
+            boolean result = instance.remove(target);
+            assertTrue(expResult == result);
+        }
+        
+        @Test
+        @DisplayName("removing item from tail")
+        public void removingItemFromTail() {
+            Container target = new Container("name", 10);
+            SinglyLinkedList<Container> instance = new SinglyLinkedList();
+            instance.add(new Container("name", 8));
+            instance.add(new Container("name2", 9));
+            instance.add(target);
+            boolean expResult = true;
+            boolean result = instance.remove(target);
+            assertTrue(expResult == result);
+        }
     }
 }
