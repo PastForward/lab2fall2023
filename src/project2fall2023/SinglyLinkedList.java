61dfd50b71e26612b15db3ffbbc8bbb6c9b8efe0
Aron Kabai Tokes <aronkt3458@hotmail.com>
2023-11-01 13:04:45 -0400

    formatting

--- src/project2fall2023/SinglyLinkedList.java
+++ src/project2fall2023/SinglyLinkedList.java
@@ -152,33 +152,32 @@ public class SinglyLinkedList<E extends Comparable<E>> {
 */
     // This shouldn't need to be here.
     // TODO: FIX THIS!!!
-@SuppressWarnings("unchecked")
-public Lister<E> iterator() {
-    // declare variables
-    Node headOfListToReturn; // beginning of new "copied" list
-    Node cursorOfListToCopy; // active node of list to copy
-    Node lastNodeOfListToReturn; // end of new "copied" list
-    // establish the copied list
-    headOfListToReturn = null;
-    if (head != null) {
-        // create the head of the new list
-        headOfListToReturn = new Node(head.getData(), null);
-        // use lastNodeOfListToReturn as a pointer to the last node in the copied list
-        lastNodeOfListToReturn = headOfListToReturn;
-        // use currentCursor as the pointer to the existing list
-        cursorOfListToCopy = head.getNext();
-        // if we have a node...
-        while (cursorOfListToCopy != null) {
-            // create a new node from the end of the new list
-            lastNodeOfListToReturn.setNext(new
-            Node(cursorOfListToCopy.getData(), null));
-            // move lastNodeOfListToReturn to the new last node
-            lastNodeOfListToReturn = lastNodeOfListToReturn.getNext();
-            // move the cursorOfListToCopy to the next node
-            cursorOfListToCopy = cursorOfListToCopy.getNext();
+    @SuppressWarnings("unchecked")
+    public Lister<E> iterator() {
+        // declare variables
+        Node headOfListToReturn; // beginning of new "copied" list
+        Node cursorOfListToCopy; // active node of list to copy
+        Node lastNodeOfListToReturn; // end of new "copied" list
+        // establish the copied list
+        headOfListToReturn = null;
+        if (head != null) {
+            // create the head of the new list
+            headOfListToReturn = new Node(head.getData(), null);
+            // use lastNodeOfListToReturn as a pointer to the last node in the copied list
+            lastNodeOfListToReturn = headOfListToReturn;
+            // use currentCursor as the pointer to the existing list
+            cursorOfListToCopy = head.getNext();
+            // if we have a node...
+            while (cursorOfListToCopy != null) {
+                // create a new node from the end of the new list
+                lastNodeOfListToReturn.setNext(new
+                Node(cursorOfListToCopy.getData(), null));
+                // move lastNodeOfListToReturn to the new last node
+                lastNodeOfListToReturn = lastNodeOfListToReturn.getNext();
+                // move the cursorOfListToCopy to the next node
+                cursorOfListToCopy = cursorOfListToCopy.getNext();
+            }
         }
+    return new Lister(headOfListToReturn);
     }
-return new Lister(headOfListToReturn);
-}
-
 }
